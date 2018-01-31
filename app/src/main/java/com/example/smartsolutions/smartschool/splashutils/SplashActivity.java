c7e0eef37e8d760c5d9a1acbce6b3c3aef757310
smartschools1996@gmail.com <smartschools1996@gmail.com>
2018-01-31 15:12:49 +0200

    first commit

---
+++ app/src/main/java/com/example/smartsolutions/smartschool/splashutils/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.example.smartsolutions.smartschool.splashutils;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.smartsolutions.smartschool.R;
+
+public class SplashActivity extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+//smart-school-5db81
+    }
+}
