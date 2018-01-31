3fbff71bd796e4eb08fabcfb29680f1b3e7f0866
smartschools1996@gmail.com <smartschools1996@gmail.com>
2018-01-31 16:09:48 +0200

    second commit

---
+++ app/src/main/java/com/example/smartsolutions/signuputil/SignupActivity.java
@@ -0,0 +1,19 @@
+package com.example.smartsolutions.signuputil;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.smartsolutions.smartschool.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_signup);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/smartsolutions/splashutil/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.example.smartsolutions.splashutil;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.smartsolutions.smartschool.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+    }
+}
