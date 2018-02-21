00fb1e002ac638a500da611d5f700d40e071f034
MMenem <mmoniemfahmy@gmail.com>
2018-02-21 15:29:35 +0200

    Signup issue(Solved)

---
+++ app/src/main/java/com/example/smartsolutions/studentutil/StudentHomeActivity.java
@@ -0,0 +1,22 @@
+package com.example.smartsolutions.studentutil;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.smartsolutions.smartschool.R;
+
+/**
+ * Created by MMenem on 2/21/2018.
+ */
+
+public class StudentHomeActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_student_home);
+    }
+}
