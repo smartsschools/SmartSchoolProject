b14d8989cf321dc240b8c7bd5d568b211b10f4d8
lody <khaldamyn140@gmail.com>
2018-02-26 12:04:05 -0800

    kh

--- app/src/main/java/com/example/smartsolutions/splashutil/SplashActivity.java
+++ app/src/main/java/com/example/smartsolutions/splashutil/SplashActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.example.smartsolutions.loginutil.LoginActiviy;
 import com.example.smartsolutions.signuputil.SignupActivity;
@@ -22,7 +23,7 @@ public class SplashActivity extends AppCompatActivity {
             public void run() {
                 Intent loginActivity=new Intent(SplashActivity.this,LoginActiviy.class);
 
-
+                Toast.makeText(SplashActivity.this, "kh", Toast.LENGTH_SHORT).show();
                 startActivity(loginActivity);
 //                Intent signupActivity=new Intent(SplashActivity.this,SignupActivity.class);
 //
